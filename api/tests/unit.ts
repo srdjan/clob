aa9f817205747599f22f8cc016e0f2ae730e164a
Srdjan Strbanovic <srdjan@timok.com>
2020-09-07 12:39:56 -0400

    added test for Traders.ts

--- api/tests/unit.ts
+++ api/tests/unit.ts
@@ -4,16 +4,28 @@ import * as Market from '../src/market'
 import * as Traders from '../src/traders'
 import { log } from '../src/utils'
 
-const test = suite('test')
-
-test('Traders: getOrCreate', () => {
+// ---- Traders --------
+const testTraders = suite('Test Traders')
+testTraders('getOrCreate', () => {
   let trader = Traders.getOrCreate('traderjoe')
 
   assert.equal(trader.username, 'traderjoe')
   assert.equal(trader.password, 'todo')
 })
+testTraders('verify: success', () => {
+  let trader = Traders.getOrCreate('joe')
+  let verified = Traders.verify('joe')
+  assert.equal(verified, true)
+})
+testTraders('verify: fail', () => {
+  Traders.getOrCreate('joe')
+  assert.throws(() => Traders.verify('sam'))
+})
+testTraders.run()
 
-test('Market: make first Buy Order', () => {
+// ---- Market --------
+const testMarket = suite('Test Market')
+testMarket('Market: make first Buy Order', () => {
   let [trade, order] = Market.bid('treaderjoe', 'Buy', 'TW', 10, 100)
 
   assert.equal(trade.ticker, 'TW')
@@ -22,7 +34,7 @@ test('Market: make first Buy Order', () => {
   assert.equal(trade.message, 'NoOrder')
 })
 
-test('Market: make first Sell Order', () => {
+testMarket('Market: make first Sell Order', () => {
   let [trade, order] = Market.bid('treaderjoe', 'Sell', 'TW', 10, 100)
 
   assert.equal(trade.ticker, 'TW')
@@ -31,7 +43,7 @@ test('Market: make first Sell Order', () => {
   assert.equal(trade.message, 'NoOrder')
 })
 
-test('Market: Cancels order', () => {
+testMarket('Market: Cancels order', () => {
   let [trade, order] = Market.bid('treaderjoe', 'Sell', 'TW', 10, 100)
   let savedOrder = Market.findOrder(order.id)
   const output = JSON.stringify(savedOrder)
@@ -44,4 +56,5 @@ test('Market: Cancels order', () => {
   // assert.equal(result, true)
 })
 
-test.run()
+testMarket.run()
+
