40046ce4a05363bbc6694f25792a0f1e8ba073c1
Srdjan Strbanovic <srdjan@timok.com>
2020-09-09 22:35:07 -0400

    added acceptance test folder

--- api/tests/acceptance/spec.ts
+++ api/tests/acceptance/spec.ts
@@ -1,25 +1,26 @@
 import { suite } from 'uvu'
 import * as assert from 'uvu/assert'
 import * as Clob from '../../src/index'
+import {MarketResponse} from '../../src/model'
+const test = suite('Acceptance test')
 
-const test = suite('test')
-
-test.skip('Buy', () => {
-  let response = Clob.bid('traderjoe', 'TW', 'Buy', 10000, 10)
-  let result = JSON.parse(response)
-  assert.equal(result.outcome, undefined)
-
-  let order = JSON.parse(response)
-  assert.equal(order.ticker, 'TW')
+test('A single valid order is accepted into the limit order book Given an empty orderbook for "TW"', () => {
+  let response = Clob.bid('trader1', 'TW', 'Buy', 9950, 100)
+  let result = JSON.parse(response) as MarketResponse
+  
+  assert.equal(result.order.ticker, 'TW')
+  assert.equal(result.order.trader.username, 'trader1')
 })
 
-test.skip('Sell', () => {
-  let json = Clob.bid('traderjoe', 'TW', 'Sell', 9999, 10)
-  let result = JSON.parse(json)
-  assert.equal(result.outcome, undefined)
+test.only('Multiple valid orders are accepted into the limit order book Given an empty orderbook for "TW"', () => {
+  let response1 = Clob.bid('trader1', 'TW', 'Buy', 9950, 100)
+  let response2 = Clob.bid('trader2', 'TW', 'Sell', 9960, 200)
+  let orderBook = Clob.getEntries('TW')
+
+  assert.equal(orderBook.length, 2)
 })
 
-test.skip('Cancel', () => {
+test('Cancel', () => {
   let response = Clob.bid('traderjoe', 'TW', 'Buy', 10000, 10)
   let order = JSON.parse(response)
 
